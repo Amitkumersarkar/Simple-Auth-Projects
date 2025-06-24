adcfa4f4c2295b932def84baf1388d7ce5885f26
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-24 12:32:41 +0600

    added new file

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,17 +1,25 @@
 
 const Register = () => {
-
+    const handleRegister = (e) => {
+        e.preventDefault();
+        // console.log(e.target.email.value);
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+    }
     return (
-        <div className="bg-cyan-900 flex items-center justify-center min-h-screen">
-            <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
+        <div className="bg-dark flex items-center justify-center min-h-screen">
+            <div className="bg-blue-400 p-6 rounded-lg shadow-md w-full max-w-sm">
                 <h1 className="text-xl font-semibold mb-4 text-center">Create an Account</h1>
 
-                <form>
+                <form onSubmit={handleRegister}>
                     <div className="mb-4">
                         <label className="block mb-1 text-sm text-gray-600">Name</label>
                         <input
                             type="text"
                             placeholder="Your Name"
+                            name="name"
                             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
                             required
                         />
@@ -21,6 +29,7 @@ const Register = () => {
                         <label className="block mb-1 text-sm text-gray-600">Email</label>
                         <input
                             type="email"
+                            name="email"
                             placeholder="you@example.com"
                             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
                             required
@@ -31,6 +40,7 @@ const Register = () => {
                         <label className="block mb-1 text-sm text-gray-600">Password</label>
                         <input
                             type="password"
+                            name="password"
                             placeholder="********"
                             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
                             required
